5fa0f1c746ec0c32fe3161d219564778a46a66f5
miyamon <ars.amnoskmkz@gmail.com>
2015-10-31 13:21:02 +0900

    add loadCustomRefreshContents

--- GradationPullToRefresh/ViewController.swift
+++ GradationPullToRefresh/ViewController.swift
@@ -9,14 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    var tblDemo: UITableView!
+    var customView: UIView!
+    var labelsArray: Array<UILabel> = []
+    
     var dataArray: Array<String> = ["A","B","C","D"]
+    var refreshControll: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        tblDemo = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        tblDemo.registerClass(UITableViewCell.self, forCellReuseIdentifier: "idCell")
+        
         tblDemo.delegate = self
         tblDemo.dataSource = self
+        
+        refreshControll = UIRefreshControl()
+        refreshControll.backgroundColor = UIColor.greenColor()
+        refreshControll.tintColor = UIColor.whiteColor()
+        
+        tblDemo.addSubview(refreshControll)
+        self.view.addSubview(tblDemo)
+        
+        loadCustomRefreshContents()
+    }
+    
+    func loadCustomRefreshContents(){
+        let refreshContents = NSBundle.mainBundle().loadNibNamed("RefreshContents", owner: self, options: nil)
+        customView = refreshContents[0] as! UIView
+        customView.frame = refreshControll.bounds
+        refreshControll.addSubview(customView)
+    }
+    
+    func pullToRefreshGradationColor(){
+        
+    
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -28,7 +61,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath:  indexPath) 
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath:  indexPath) as UITableViewCell
         
         cell.textLabel!.text = dataArray[indexPath.row]
         
