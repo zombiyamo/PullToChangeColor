fb089b3cd810269cd0d8d3d83392529cf642f42b
miyamon <ars.amnoskmkz@gmail.com>
2015-10-31 11:44:59 +0900

    first commit

--- GradationPullToRefresh/ViewController.swift
+++ GradationPullToRefresh/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var dataArray: Array<String> = ["A","B","C","D"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tblDemo.delegate = self
+        tblDemo.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath:  indexPath) 
+        
+        cell.textLabel!.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
     }
 
     override func didReceiveMemoryWarning() {
