43dc168fb289f155e76d267789dd7ccc0fa096f0
miyamon <ars.amnoskmkz@gmail.com>
2015-10-31 11:37:39 +0900

    Initial Commit

---
+++ GradationPullToRefreshTests/GradationPullToRefreshTests.swift
@@ -0,0 +1,36 @@
+//
+//  GradationPullToRefreshTests.swift
+//  GradationPullToRefreshTests
+//
+//  Created by miyamo on 2015/10/31.
+//  Copyright © 2015年 miyamoto haruna. All rights reserved.
+//
+
+import XCTest
+@testable import GradationPullToRefresh
+
+class GradationPullToRefreshTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
